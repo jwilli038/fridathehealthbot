e306e9a237d8c2545c2d93d3763b7db7503f731b
jwilli038 <jwilli038@gmail.com>
2022-06-27 06:38:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # libraries some notes
 import random
 import numpy as np
-import pickle
+import pickle5 as pickle
 import json
 from flask import Flask, render_template, request
 #from flask_ngrok import run_with_ngrok
